32c81783dd6537664a9fd4ab6c3ee5c39aea9a9e
klim <klimworkss@gmail.com>
2024-03-05 08:32:58 +0200

    2.1 patch system library

--- module21/upload/system/library/qwqer/QwqerApi.php
+++ module21/upload/system/library/qwqer/QwqerApi.php
@@ -87,15 +87,29 @@ class QwqerApi {
     }
     private $registry;
     private  $trade_pt;
-    private  $weburl                = "https://qwqer.hostcream.eu/api/v1/plugins/opencart";
+
+    private  $entry_url = "https://qwqer.hostcream.eu";
+    private  $weburl                = "/api/v1";
+
+    private $prefix                 = "/plugins/open-cart";
+
+    /**
+     * @return string
+     */
+    public function getWeburl(): string
+    {
+        return $this->entry_url;
+    }
     private  $autocompleteUrl       = '/places/autocomplete';
     private  $geoCodeUrl            = '/places/geocode';
     private  $getPriceUrl           = '/clients/auth/trading-points/{trading_points}/delivery-orders/get-price';
     private  $getParcelMachinesUrl  = '/parcel-machines';
+    private  $getDeliveryOrders     = '/delivery-orders/${id}?include=places';
     private  $createOrderUrl;
 
     public function __construct($registry)
     {
+        $this->weburl = $this->entry_url . $this->weburl . $this->prefix;
 
         $this->token    = $registry->get('config')->get('qwqer_api');
         $this->trade_pt = $registry->get('config')->get('qwqer_trade_pt');
@@ -105,6 +119,7 @@ class QwqerApi {
         $this->getPriceUrl          = $this->weburl . $this->getPriceUrl;
         $this->createOrderUrl       = $this->weburl . '/clients/auth/trading-points/' . $this->trade_pt . '/delivery-orders';
         $this->getParcelMachinesUrl = $this->weburl . $this->getParcelMachinesUrl;
+        $this->getDeliveryOrders    = $this->entry_url . '/api/v1' . $this->getDeliveryOrders;
 
         $this->getPriceUrl  = str_replace(['{trading_points}'],[$this->trade_pt],$this->getPriceUrl);
         $registry->set('shipping_qwqer',$this);
@@ -117,6 +132,14 @@ class QwqerApi {
         }
         $this->delivery_types = $ret;
         
+    }
+
+
+    /* HELPERS */
+    function isJson($string) {
+        json_decode($string, true);
+        $return =  json_last_error() === JSON_ERROR_NONE;
+        return $return;
     }
 
     /**
@@ -133,39 +156,6 @@ class QwqerApi {
         ]);
 
         return $curl;
-    }
-
-    //!!!Api
-
-    /**  Geocode for  place
-     * @link https://qwqer-api-docs.netlify.app/api/endpoints/places#address-geocoding
-     * @param $adress
-     * @return array
-     */
-    public function getGeoCode($address,$city='riga',$locality='lv')
-    {
-        $curl = $this->getCurlHandle();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL             => $this->geoCodeUrl,
-            CURLOPT_RETURNTRANSFER  => true,
-            CURLOPT_ENCODING        => '',
-            CURLOPT_MAXREDIRS       => 10,
-            CURLOPT_TIMEOUT         => 0,
-            CURLOPT_FOLLOWLOCATION  => true,
-            CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST   => 'POST',
-            CURLOPT_POSTFIELDS => array('address' => $address, 'country'=>$locality, 'locality'=>$city),
-
-        ));
-
-
-        $response = curl_exec($curl);
-        $r = mb_detect_encoding($response);
-        $response = mb_convert_encoding($response,$r,'utf-8');
-        curl_close($curl);
-        $response = json_decode($response,true);
-        return $response;
     }
 
 
@@ -200,7 +190,7 @@ class QwqerApi {
 
         $shipping_phone = '+371'.preg_replace(array('/\s/m','/^\+/m','/^\+371/m','/^371/m'),array('','','',''),$shipping_phone);
 
-         /* Get client coordinates and address */
+        /* Get client coordinates and address */
         $data_info_client =  $address['address_1'] . ' ' . $address['address_2'];
 
         $address_city = mb_strtolower($address['city']);
@@ -245,6 +235,41 @@ class QwqerApi {
         return  $data_orders;
 
     }
+
+    //!!!Api
+
+    /**  Geocode for  place
+     * @link https://qwqer-api-docs.netlify.app/api/endpoints/places#address-geocoding
+     * @param $adress
+     * @return array
+     */
+    public function getGeoCode($address,$city='riga',$locality='lv')
+    {
+        $curl = $this->getCurlHandle();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL             => $this->geoCodeUrl,
+            CURLOPT_RETURNTRANSFER  => true,
+            CURLOPT_ENCODING        => '',
+            CURLOPT_MAXREDIRS       => 10,
+            CURLOPT_TIMEOUT         => 0,
+            CURLOPT_FOLLOWLOCATION  => true,
+            CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST   => 'POST',
+            CURLOPT_POSTFIELDS => array('address' => $address, 'country'=>$locality, 'locality'=>$city),
+
+        ));
+
+
+        $response = curl_exec($curl);
+        $r = mb_detect_encoding($response);
+        $response = mb_convert_encoding($response,$r,'utf-8');
+        curl_close($curl);
+        $response = json_decode($response,true);
+        return $response;
+    }
+
+
 
     /** calculate price for a delivery
      * @link  https://qwqer-api-docs.netlify.app/api/endpoints/delivery-orders#calculate-price-of-order-before-create-them
@@ -348,6 +373,7 @@ class QwqerApi {
     }
 
     /** Get parcel machine terminals
+     * @link https://qwqer-api-docs.netlify.app/api/endpoints/
      * @return array
      */
     public function getParcelTerminals(){
@@ -374,13 +400,40 @@ class QwqerApi {
 
     }
 
+    public function getDeliveryOrder($remote_id){
+        $url = $this->getDeliveryOrders;
+        $url = str_replace('${id}',$remote_id,$url);
+
+        $curl = $this->getCurlHandle();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL             => $url,
+            CURLOPT_RETURNTRANSFER  => true,
+            CURLOPT_ENCODING        => '',
+            CURLOPT_MAXREDIRS       => 10,
+            CURLOPT_TIMEOUT         => 0,
+            CURLOPT_FOLLOWLOCATION  => true,
+            CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST   => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+        $r = mb_detect_encoding($response);
+        $response = mb_convert_encoding($response,$r,'utf-8');
+        curl_close($curl);
+        $response = json_decode($response,true);
+        return $response;
+
+    }
+
+    /* DATABASE FUNCTIONS*/
+
     public function addResponseRecord($response,$order_id){
         if (isset($response) && $response){
             $data = json_encode($response);
-            $str = "UPDATE " . DB_PREFIX . "qwqer_data set `response` = '" . $this->registry->get('db')->escape($data) . "' where `order_id` = {$order_id};";
+            $mysqldate = $mysqldate = date( 'Y-m-d H:i:s' );
+            $str = "UPDATE " . DB_PREFIX . "qwqer_data set `response` = '" . $this->registry->get('db')->escape($data) . "', `qwqer_date`='{$mysqldate}' where `order_id` = {$order_id};";
             $this->registry->get('db')->query($str);
         }
-
     }
 
     public function getResponse($order_id){
@@ -396,5 +449,78 @@ class QwqerApi {
         return $ret;
     }
 
+    public function getOrdersList($data=array()){
+        $conditions = '';
+        if (isset($data['order_id'])){
+            $conditions = " order_id = {$data['order_id']}";
+        }
+
+        if ($conditions){
+            $conditions = ' where '.$conditions;
+        }
+
+
+        if (isset($data['limit'])){
+            $page = 1;
+            if (isset($data['page'])){
+                $page = $data['page'];
+            }
+            $offset = ($page-1)*$data['limit'];
+            $limits = " LIMIT {$data['limit']} OFFSET {$offset}";
+        }
+
+        $order = " ORDER BY `qwqer_date` ASC ";
+
+        $qry  = "SELECT * FROM ". DB_PREFIX . "qwqer_data ";
+        $qry .= $conditions.$order.$limits;
+
+        $res = $this->registry->get('db')->query($qry)->rows;
+        $ret = array();
+        foreach ($res as $val){
+            if (isset($val['data']) && $val['data'] && $this->isJson($val['data'])){
+                $order_data = json_decode($val['data'],true);
+            }else{
+                $order_data = array();
+            }
+            if (isset($val['response']) && $val['response'] && $this->isJson($val['response'])){
+                $resp_data = json_decode($val['response'],true);
+            }else{
+                $resp_data = array();
+            }
+
+            $order_link = false;
+
+
+            $ret[] = array(
+                'qwqer_id'   => $val['qwqer_id'],
+                'key_hash'   => $val['key_hash'],
+                'order_id'   => $val['order_id'],
+                'response'   => $resp_data,
+                'data'       => $order_data,
+                'qwqer_date' => $val['qwqer_date']
+            );
+
+        }
+
+        return $ret;
+
+    }
+
+
+    /**Deletes an order
+     * @param $qwqer_id
+     * @return void
+     */
+    public function deleteOrder($qwqer_id){
+        $this->registry->get('db')->query("DELETE FROM ". DB_PREFIX . "qwqer_data WHERE `qwqer_id` = {$qwqer_id}");
+    }
+
+    /**Calculates an order
+     * @param $qwqer_id
+     * @return void
+     */
+    public function getOrdersCount(){
+        return $this->registry->get('db')->query("SELECT COUNT(*) as `count` FROM ". DB_PREFIX . "qwqer_data")->rows[0]['count'];
+    }
 
 }
