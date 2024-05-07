25d1ce551956f27f29d032111c3d60cefbea3aa5
klim <klimworkss@gmail.com>
2024-05-07 15:23:27 +0300

    add teste

--- tests/src/buy.test.js
+++ tests/src/buy.test.js
@@ -1,9 +1,14 @@
+import navigator from "./opencart/navigation";
+import config from "./opencart/configs";
+
 const timeout = 15000; 
-beforeAll(async () => {await page.goto(URL, {waitUntil: 'domcontentloaded'});
+beforeAll(async () => {
   
 }); 
-describe('Test page title and header', () => { 
+describe('product working', () => { 
 test('page title', async () => { 
+  
+  await navigator.goto(config.shops.opencart.ocs23, config.shops.opencart.products.iPhone);
+  
   const title = await page.title(); expect(title).toBe('Testing'); }, timeout); 
-
 });
---
+++ tests/src/opencart/navigation.js
@@ -0,0 +1,10 @@
+const navigator = {
+    routes:{
+        iPhone: 'index.php?route=product/product&product_id=40'
+    },
+    goto: async(path, where)=>{
+        await page.goto(path + where, {waitUntil: 'domcontentloaded'});
+    },
+}
+
+export default navigator;
